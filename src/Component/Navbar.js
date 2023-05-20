2606ad438862e97b53bc2caff4b6d4ff888d19d3
sourabh gahlot <sourabhgahlot9826@gmail.com>
2023-05-20 15:05:36 +0530

    mayank bhai ke changes

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
             {/* <SideBarFinal/> */}
           </div>
           <div className='col-md-6 d-none d-md-block header_center_text'>
-            <h2 className='mt-3 ml-1 text-center font-weight-normal d-none d-lg-block'>Aurbitu Management System</h2>
+            <h2 className='mt-3 ml-1 text-center font-weight-normal d-none d-lg-block'>Mayank</h2>
             <h3 className='mt-3 ml-1 text-center font-weight-normal d-none d-md-block d-lg-none'>Aurbitus Management System</h3>
           </div>
           <div className='col-md-3 pl-2 mt-1  d-none d-md-block'>
