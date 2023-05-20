6d73d2de0f3b78706c4f64fb88be7c019f171a76
sourabh gahlot <sourabhgahlot9826@gmail.com>
2023-05-20 15:10:40 +0530

    mayank bhai ki testing

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
       
         <div className='row'>
           <div className='col-6 col-md-2 header_logo_text'>
-            <h1 className='mt-3 ml-1'>AMH</h1>
+            <h1 className='mt-3 ml-1'>A</h1>
             {/* <SideBarFinal/> */}
           </div>
           <div className='col-md-6 d-none d-md-block header_center_text'>
